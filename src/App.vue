95c830ce40062611ee6ade75618ab65fa6115725
jdrabant <jakob-drabant>
2021-10-15 00:37:41 +0200

    Append symbols to display

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,50 @@
   <div id="app">
     <input type="text" class="display" value="0">
     <div class="container">
-      <button class="greyBtn grid-item grid-item1">AC</button>
+      <button v-on:click="resetValue()" class="greyBtn grid-item grid-item1">AC</button>
       <button class="greyBtn grid-item grid-item2">+/-</button>
       <button class="greyBtn grid-item grid-item3">%</button>
-      <button class="orangeBtn grid-item grid-item4">/</button>
-      <button class="grid-item grid-item5">7</button>
-      <button class="grid-item grid-item6">8</button>
-      <button class="grid-item grid-item7">9</button>
-      <button class="orangeBtn grid-item grid-item8">x</button>
-      <button class="grid-item grid-item9">4</button>
-      <button class="grid-item grid-item10">5</button>
-      <button class="grid-item grid-item11">6</button>
-      <button class="orangeBtn grid-item grid-item12">-</button>
-      <button class="grid-item grid-item13">1</button>
-      <button class="grid-item grid-item14">2</button>
-      <button class="grid-item grid-item15">3</button>
-      <button class="orangeBtn grid-item grid-item16">+</button>
-      <button class="grid-item grid-item17">0</button>
-      <button class="grid-item grid-item18">,</button>
+      <button v-on:click="appendToDisplay('/')" class="orangeBtn grid-item grid-item4">/</button>
+      <button v-on:click="appendToDisplay('7')" class="grid-item grid-item5">7</button>
+      <button v-on:click="appendToDisplay('8')" class="grid-item grid-item6">8</button>
+      <button v-on:click="appendToDisplay('9')" class="grid-item grid-item7">9</button>
+      <button v-on:click="appendToDisplay('*')" class="orangeBtn grid-item grid-item8">*</button>
+      <button v-on:click="appendToDisplay('4')" class="grid-item grid-item9">4</button>
+      <button v-on:click="appendToDisplay('5')" class="grid-item grid-item10">5</button>
+      <button v-on:click="appendToDisplay('6')" class="grid-item grid-item11">6</button>
+      <button v-on:click="appendToDisplay('-')" class="orangeBtn grid-item grid-item12">-</button>
+      <button v-on:click="appendToDisplay('1')" class="grid-item grid-item13">1</button>
+      <button v-on:click="appendToDisplay('2')" class="grid-item grid-item14">2</button>
+      <button v-on:click="appendToDisplay('3')" class="grid-item grid-item15">3</button>
+      <button v-on:click="appendToDisplay('+')" class="orangeBtn grid-item grid-item16">+</button>
+      <button v-on:click="appendToDisplay('0')" class="grid-item grid-item17">0</button>
+      <button v-on:click="appendToDisplay(',')" class="grid-item grid-item18">,</button>
       <button class="orangeBtn grid-item grid-item19">=</button>
     </div>
     
   </div>
 </template>
 
+
+
 <script>
+import $ from "jquery";
+
 export default {
   name: "App",
+  methods: {
+    appendToDisplay(value) {
+      const displayValue = $(".display").val();
+      if (displayValue === "0") {
+        $(".display").val(value);
+        return;
+      }
+      $(".display").val(displayValue + value);
+    },
+    resetValue() {
+      $(".display").val(0);
+    },
+  },
 };
 </script>
 
