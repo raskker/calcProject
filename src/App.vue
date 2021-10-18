22db7f424d0a2caf5ff009093dd5aa785d7434ba
jdrabant <jakob-drabant>
2021-10-18 23:50:54 +0200

    logic

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,108 @@
 <template>
-  <div id="app">
-    <input type="text" class="display" value="0">
     <div class="container">
-      <button v-on:click="resetValue()" class="greyBtn grid-item grid-item1">AC</button>
-      <button class="greyBtn grid-item grid-item2">+/-</button>
-      <button class="greyBtn grid-item grid-item3">%</button>
-      <button v-on:click="appendToDisplay('/')" class="orangeBtn grid-item grid-item4">/</button>
-      <button v-on:click="appendToDisplay('7')" class="grid-item grid-item5">7</button>
-      <button v-on:click="appendToDisplay('8')" class="grid-item grid-item6">8</button>
-      <button v-on:click="appendToDisplay('9')" class="grid-item grid-item7">9</button>
-      <button v-on:click="appendToDisplay('*')" class="orangeBtn grid-item grid-item8">*</button>
-      <button v-on:click="appendToDisplay('4')" class="grid-item grid-item9">4</button>
-      <button v-on:click="appendToDisplay('5')" class="grid-item grid-item10">5</button>
-      <button v-on:click="appendToDisplay('6')" class="grid-item grid-item11">6</button>
-      <button v-on:click="appendToDisplay('-')" class="orangeBtn grid-item grid-item12">-</button>
-      <button v-on:click="appendToDisplay('1')" class="grid-item grid-item13">1</button>
-      <button v-on:click="appendToDisplay('2')" class="grid-item grid-item14">2</button>
-      <button v-on:click="appendToDisplay('3')" class="grid-item grid-item15">3</button>
-      <button v-on:click="appendToDisplay('+')" class="orangeBtn grid-item grid-item16">+</button>
-      <button v-on:click="appendToDisplay('0')" class="grid-item grid-item17">0</button>
-      <button v-on:click="appendToDisplay(',')" class="grid-item grid-item18">,</button>
-      <button class="orangeBtn grid-item grid-item19">=</button>
+      <div class="display">{{current || '0'}}</div>
+      <div @click="resetValue()" class="greyBtn grid-item grid-item1">AC</div>
+      <div class="greyBtn grid-item grid-item2">+/-</div>
+      <div class="greyBtn grid-item grid-item3">%</div>
+      <div @click="divide" class="orangeBtn grid-item grid-item4">/</div>
+      <div @click="appendToDisplay('7')" class="grid-item grid-item5">7</div>
+      <div @click="appendToDisplay('8')" class="grid-item grid-item6">8</div>
+      <div @click="appendToDisplay('9')" class="grid-item grid-item7">9</div>
+      <div @click="multi" class="orangeBtn grid-item grid-item8">*</div>
+      <div @click="appendToDisplay('4')" class="grid-item grid-item9">4</div>
+      <div @click="appendToDisplay('5')" class="grid-item grid-item10">5</div>
+      <div @click="appendToDisplay('6')" class="grid-item grid-item11">6</div>
+      <div @click="minus" class="orangeBtn grid-item grid-item12">-</div>
+      <div @click="appendToDisplay('1')" class="grid-item grid-item13">1</div>
+      <div @click="appendToDisplay('2')" class="grid-item grid-item14">2</div>
+      <div @click="appendToDisplay('3')" class="grid-item grid-item15">3</div>
+      <div @click="plus" class="orangeBtn grid-item grid-item16">+</div>
+      <div @click="appendToDisplay('0')" class="grid-item grid-item17">0</div>
+      <div @click="dot()" class="grid-item grid-item18">.</div>
+      <div @click="equal()" class="orangeBtn grid-item grid-item19">=</div>
     </div>
-    
-  </div>
 </template>
 
 
 
 <script>
-import $ from "jquery";
-
 export default {
   name: "App",
+  data() {
+    return {
+      current: "",
+      operator: null,
+      previous: null,
+      operatorClicked: false,
+    };
+  },
   methods: {
     appendToDisplay(value) {
-      const displayValue = $(".display").val();
-      if (displayValue === "0") {
-        $(".display").val(value);
-        return;
+      if (this.operatorClicked) {
+        this.current = "";
+        this.operatorClicked = false;
       }
-      $(".display").val(displayValue + value);
+      this.current = `${this.current}${value}`;
     },
     resetValue() {
-      $(".display").val(0);
+      this.current = "";
+    },
+    calculateValue() {},
+    dot() {
+      if (this.current.indexOf(".") === -1) {
+        this.appendToDisplay(".");
+      }
+    },
+    setPrevious() {
+      this.previous = this.current;
+      this.operatorClicked = true;
+    },
+    divide() {
+      this.operator = (a, b) => a / b;
+      this.setPrevious();
+    },
+    minus() {
+      this.operator = (a, b) => a - b;
+      this.setPrevious();
+    },
+    plus() {
+      this.operator = (a, b) => a + b;
+      this.setPrevious();
+    },
+    multi() {
+      this.operator = (a, b) => a * b;
+      this.setPrevious();
+    },
+    equal() {
+      this.current = `${this.operator(
+        parseFloat(this.current),
+        parseFloat(this.previous)
+      )}`;
+      this.previous = null;
     },
   },
 };
 </script>
 
 <style>
-#app {
-  max-width: 320px;
-}
-
 .container {
-  margin: 5px;
+  margin: 0 auto;
+  width: 400px;
+  font-size: 40px;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
-  grid-auto-rows: 80px;
-  grid-gap: 2px;
+  grid-auto-rows: minmax(50px, auto);
 }
 
 .grid-item {
   border: 1px solid black;
-  border-radius: 10px;
   text-align: center;
   line-height: 80px;
   vertical-align: middle;
 }
 
 .display {
+  grid-column: 1 / 5;
   width: 100%;
   height: 100px;
   border: 1px solid black;
